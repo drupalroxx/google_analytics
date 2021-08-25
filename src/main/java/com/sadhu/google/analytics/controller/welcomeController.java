d52b30d5eb505ef5570a40b4ceffb68f8e76f7ac
sadhana <sadhana.maurya@gmail.com>
2021-08-25 23:17:44 +0100

    added request parameter to welcome msg

--- src/main/java/com/sadhu/google/analytics/controller/welcomeController.java
+++ src/main/java/com/sadhu/google/analytics/controller/welcomeController.java
@@ -3,20 +3,23 @@ package com.sadhu.google.analytics.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class welcomeController {
+
     @RequestMapping(value = "/welcome", method = RequestMethod.GET)
-    public String welcome() {
-        return "welcome to sadhanas google analytics";
+    public String welcome(@RequestParam String name) {
+        return "welcome to "+name+" google analytics";
     }
     @RequestMapping(value = "/namaste", method = RequestMethod.GET)
-    public String namaste(){
-        return "namaste apka swagat hai sadhana ke google analytics me";
+    public String namaste(@RequestParam("name") String name, @RequestParam("age") int age){
+        return "namaste "+name+" and "+age+" apka swagat hai sadhana ke google analytics me";
     }
     @RequestMapping(value = "/greetings", method = RequestMethod.GET)
-    public String greetings() { return "greetings from google analytics";}
+    public String greetings(@RequestParam("name") String name, @RequestParam("age") int age, @RequestParam("sex") String sex)
+    { return "hello "+name+" withe age "+age+" and the sex "+sex+" greetings from google analytics";}
 
 
 }
