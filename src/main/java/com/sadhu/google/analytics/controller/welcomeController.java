f86214422441ed264e3695f3b71ec66d236a65aa
sadhana <sadhana.maurya@gmail.com>
2021-08-31 06:53:27 +0100

    update post added

--- src/main/java/com/sadhu/google/analytics/controller/welcomeController.java
+++ src/main/java/com/sadhu/google/analytics/controller/welcomeController.java
@@ -1,6 +1,8 @@
 package com.sadhu.google.analytics.controller;
 
+import com.sadhu.google.analytics.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,5 +23,13 @@ public class welcomeController {
     public String greetings(@RequestParam("name") String name, @RequestParam("age") int age, @RequestParam("sex") String sex)
     { return "hello "+name+" withe age "+age+" and the sex "+sex+" greetings from google analytics";}
 
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String welcomeUser(@RequestBody User user){
+        return "welcome "+user.getName()+" with the age "+user.getAge()+" and sex "+user.getSex();
+    }
+
+
 
 }
+
+
